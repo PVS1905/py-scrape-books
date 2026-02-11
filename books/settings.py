aa05885d59b10154a4760db7da871463c1db11e3
P.V.S.1905 <viktor66462@gmail.com>
2026-02-11 03:26:11 +0200

    Solution_corrected

--- books/settings.py
+++ books/settings.py
@@ -35,7 +35,6 @@ DOWNLOAD_DELAY = 1
 # Disable cookies (enabled by default)
 # COOKIES_ENABLED = False
 
-# Disable Telnet Console (enabled by default)
 # TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
