d7c9bcfc5d8ae652c75889c6ec3bc47a2f9d3ad1
P.V.S.1905 <viktor66462@gmail.com>
2026-02-11 03:50:21 +0200

    Fix_setting

--- books/settings.py
+++ books/settings.py
@@ -61,12 +61,6 @@ DOWNLOAD_DELAY = 1
 #    "scrapy.extensions.telnet.TelnetConsole": None,
 # }
 
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    "books.pipelines.BooksPipeline": 300,
-# }
-
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 # AUTOTHROTTLE_ENABLED = True
